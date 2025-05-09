2203c2696b21619e78392579687da5142c953de9
Areadomus <areadomusgit@gmail.com>
2025-05-09 09:00:50 +0200

    Update alg.py

--- alg.py
+++ alg.py
@@ -43,19 +43,19 @@ def assign_priority(cleaners, apartments, priority_level, previous_assignments):
     assignments = []
     
     for cleaner in cleaners:
-    suitable_apts = [a for a in apartments if
-                     a["type"] == cleaner["role"] and
-                     a["task_id"] not in [x["apt_id"] for x in previous_assignments]]
-    if suitable_apts:
-        apt = suitable_apts.pop(0)
-        assignments.append({
-                "cleaner_id": cleaner["id"],
-                "apt_id": apt["task_id"],
-                "priority": priority_level,
-                "start_time": "08:00",  # iniziale dummy
-                "estimated_end": "09:00"  # dummy
-        })
-    return assignments
+        suitable_apts = [a for a in apartments if
+                        a["type"] == cleaner["role"] and
+                        a["task_id"] not in [x["apt_id"] for x in previous_assignments]]
+        if suitable_apts:
+            apt = suitable_apts.pop(0)
+            assignments.append({
+                    "cleaner_id": cleaner["id"],
+                    "apt_id": apt["task_id"],
+                    "priority": priority_level,
+                    "start_time": "08:00",  # iniziale dummy
+                    "estimated_end": "09:00"  # dummy
+            })
+        return assignments
 
 
 def find_closest_apt(cleaner_last_apt, remaining_apts):
